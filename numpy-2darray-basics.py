4eed4362340a1d437936e1b9792fca7df7805c0f
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2020-03-31 22:07:52 -0400

    initial add

---
+++ numpy-2darray-basics.py
@@ -0,0 +1,16 @@
+import numpy as np
+a = np.array([[1, 2], [3, 4], [5, 6]])
+
+print("Array")
+print(a)
+
+print("Length of the array := {}".format(len(a)))
+print("Shape/Dimension if the array := {}".format(a.shape))
+print("Date type of array elements  := {}".format(a.dtype))
+
+print("Value at index 0 := {}".format(a[0]))
+print("Value at index 1 := {}".format(a[1]))
+print("Value at index 2 := {}".format(a[2]))
+
+print("Length of the array {}".format(len(a)))
+
---
+++ numpy-array-basics.py
@@ -0,0 +1,11 @@
+import numpy as np
+a = np.array([1, 2, 3])
+
+print("The array  {}".format(a))
+print("Length of the array {}".format(len(a)))
+print("Value at index 0 {}".format(a[0]))
+print("Value at index 1 {}".format(a[1]))
+print("Value at index 2 {}".format(a[2]))
+
+print("Shape/Dimension if the array {}".format(a.shape))
+print("Date type of array elements {}".format(a.dtype))
